43c54a314c4e4788439d4eadb95c9b12f2c3a4f3
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-21 19:15:50 -0300

    fix: update s3 bucket name

--- optimize.ts
+++ optimize.ts
@@ -1,35 +1,41 @@
-import * as AWS from 'aws-sdk';
-import * as sharp from 'sharp';
-import { basename, extname } from 'path';
 
-export default async function handle ({ Records: records }, context) {
+"use strict";
+
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+const { basename, extname } = require("path");
+
+const S3 = new AWS.S3();
+
+module.exports.handle = async ({ Records: records }, context) => {
   try {
-    const S3 = new AWS.S3()
+    await Promise.all(
+      records.map(async record => {
+        const { key } = record.s3.object;
 
-    await Promise.all(records.map(async record => {
-      const { key } = record.s3.object;
+        const image = await S3.getObject({
+          Bucket: process.env.bucket,
+          Key: key
+        }).promise();
 
-      const image = await S3.getObject({
-        Bucket: process.env.bucket,
-        Key: key
-      }).promise();
+        const optimized = await sharp(image.Body)
+          .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
+          .toFormat("jpeg", { progressive: true, quality: 50 })
+          .toBuffer();
 
-      const optimized = await sharp(image.Body)
-        .resize(1200, 720, { fit: 'inside', withoutEnlargement: true })
-        .toFormat('jpeg', { progressive: true, quality: 50 })
-        .toBuffer()
-
-      await S3.putObject({
-        Body: optimized,
-        Bucket: process.env.bucket,
-        ContentType: 'image/jpeg',
-        Key: `compressed/${basename(key, extname(key))}.jpg`
-      }).promise()
-    }));
+        await S3.putObject({
+          Body: optimized,
+          Bucket: process.env.bucket,
+          ContentType: "image/jpeg",
+          Key: `compressed/${basename(key, extname(key))}.jpg`
+        }).promise();
+      })
+    );
 
     return {
-      statusCode: 204,
-    }
+      statusCode: 301,
+      body: { ok: true }
+    };
   } catch (err) {
     return err;
   }
