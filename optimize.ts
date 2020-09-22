75d12a37cede74ba3b4477eb43b12f53ec7a1863
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-21 22:56:18 -0300

    feat: add s3 object types

--- optimize.ts
+++ optimize.ts
@@ -1,40 +1,39 @@
-
-"use strict";
-
-const AWS = require("aws-sdk");
-const sharp = require("sharp");
-const { basename, extname } = require("path");
+import * as AWS from 'aws-sdk';
+import * as sharp from 'sharp';
+import { basename, extname } from 'path';
 
 const S3 = new AWS.S3();
 
-module.exports.handle = async ({ Records: records }, context) => {
+export default async function handle ({ Payload }: AWS.S3.RecordsEvent) {
   try {
+    const records: any = Payload
+
     await Promise.all(
       records.map(async record => {
-        const { key } = record.s3.object;
+        const key: string  = record.s3.object.key;
+        const bucket: string = process.env.bucket || '';
 
-        const image = await S3.getObject({
-          Bucket: process.env.bucket,
+        const image: AWS.S3.GetObjectOutput = await S3.getObject({
+          Bucket: bucket,
           Key: key
         }).promise();
 
-        const optimized = await sharp(image.Body)
-          .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
-          .toFormat("jpeg", { progressive: true, quality: 50 })
+        const optimized = await sharp(<sharp.SharpOptions>image.Body)
+          .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
+          .toFormat('jpeg', { progressive: true, quality: 50 })
           .toBuffer();
 
         await S3.putObject({
           Body: optimized,
-          Bucket: process.env.bucket,
-          ContentType: "image/jpeg",
+          Bucket: bucket,
+          ContentType: 'image/jpeg',
           Key: `compressed/${basename(key, extname(key))}.jpg`
         }).promise();
       })
     );
 
     return {
-      statusCode: 301,
-      body: { ok: true }
+      statusCode: 204,
     };
   } catch (err) {
     return err;
